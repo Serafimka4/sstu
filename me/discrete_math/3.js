b7dca8b474ca4e65a587fc91489d016b11d43e04
eanmos <eanmos@ya.ru>
2019-12-16 00:24:28 +0400

    Math #3 completed.

--- me/discrete_math/3.js
+++ me/discrete_math/3.js
@@ -1,46 +1,72 @@
 'use strict';
 
-const expression = "(x && y' ^ z) | ((x -> z') <-> y)";
+const DEBUG = false;
+
+const expression = "(X ∧ ¬Y ⊕ Z) | ((X -> ¬Z) <-> Y)";
 
 function and(a, b)
 {
-	const result = a && b;
-	return result === true ? 1 : 0;
+	const result = (a && b) ? 1 : 0;
+
+	if (DEBUG)
+		console.log(`AND(${a}, ${b}) = ${result}`);
+
+	return result;
 }
 
 function not(a)
 {
-	const result = !a;
-	return result === true ? 1 : 0;
+	const result = (!a) ? 1 : 0;
+	
+	if (DEBUG)
+		console.log(`NOT(${a}, ${b}) = ${result}`);
+	
+	return result;
 }
 
 function xor(a, b)
 {
-	const result = !((a && b) || ((!a) && (!b)));
-	return result === true ? 1 : 0;
+	const result = !((a && b) || ((!a) && (!b))) ? 1 : 0;
+
+	if (DEBUG)
+		console.log(`XOR(${a}, ${b}) = ${result}`);
+
+	return result;
 }
 
 function imply(a, b)
 {
-	const result = (a && (!b)) ? false : true;
-	return result === true ? 1 : 0;
+	const result = (a && (!b)) ? 0 : 1;
+	
+	if (DEBUG)
+		console.log(`IMPLY(${a}, ${b}) = ${result}`);
+	
+	return result;
 }
 
 function eq(a, b)
 {
-	const result = a === b;
-	return result === true ? 1 : 0;
+	const result = a === b ? 1 : 0;
+	
+	if (DEBUG)
+		console.log(`EQ(${a}, ${b}) = ${result}`);
+	
+	return result;
 }
 
 function nand(a, b)
 {
-	const result = !(a && b);
-	return result === true ? 1 : 0;
+	const result = !(a && b) ? 1 : 0;
+	
+	if (DEBUG)
+		console.log(`NAND(${a}, ${b}) = ${result}`);
+	
+	return result;
 }
 
 const binaryBooleanFunctions = [
-	{char: "&&", proc: and},
-	{char: "\\^", proc: xor},
+	{char: "∧", proc: and},
+	{char: "⊕", proc: xor},
 	{char: "->", proc: imply},
 	{char: "<->", proc: eq},
 	{char: "\\|", proc: nand}
@@ -62,10 +88,13 @@ function calculateExpression(expression, variables)
 		expr = replaceAll(expr, v[0], v[1]);
 	
 	/* Negation. */
-	expr = replaceAll(expr, "0'", "1");
-	expr = replaceAll(expr, "1'", "0");
+	expr = replaceAll(expr, "¬0", "1");
+	expr = replaceAll(expr, "¬1", "0");
 
 	for (const f of binaryBooleanFunctions) {
+		if (DEBUG)
+			console.log(expr);
+
 		expr = replaceAll(expr, `(\\d)${f.char}(\\d)`, function (match, a, b, offset, string) {
 			return f.proc(Number(a), Number(b));
 		});
@@ -73,27 +102,94 @@ function calculateExpression(expression, variables)
 		/* Remove unused parenthesis. */
 		expr = replaceAll(expr, "\\((\\d)\\)", "$1");
 	}
+	
+	if (DEBUG)
+		console.log(" ");
 
 	return expr;
 }
 
 function createTruthTableForThreeVariables(expression)
 {
-	console.log(`X Y Z F`);
+	let result = "X Y Z F\n";
+	result +=    "-------";
 
 	for (let x = 0; x < 2; ++x) {
 		for (let y = 0; y < 2; ++y) {
 			for (let z = 0; z < 2; ++z) {
 				const variables = [
-					["x", x],
-					["y", y],
-					["z", z]
+					["X", x],
+					["Y", y],
+					["Z", z]
 				];
 
-				console.log(`${x} ${y} ${z} ${calculateExpression(expression, variables)}`);
+				result += `\n${x} ${y} ${z} ${calculateExpression(expression, variables)}`;
 			}
 		}
 	}
+
+	return result;
 }
 
-createTruthTableForThreeVariables(expression);
+function createCnfForThreeVariables(truthTableAsString)
+{
+	let cnf = function (x, y, z) {
+		let result = "";
+
+		result += ((x === '1') ? "¬X" : "X") + " ∨ ";
+		result += ((y === '1') ? "¬Y" : "Y") + " ∨ ";
+		result += (z === '1') ? "¬Z" : "Z";
+
+		return result;
+	};
+
+	let result = "";
+
+	let truthTable = truthTableAsString.split('\n');
+	
+	for (let i = 0; i < truthTable.length; ++i)
+		truthTable[i] = truthTable[i].split(' ');
+
+	for (let line of truthTable) {
+		if (line[3] === '0')
+			result += "(" + cnf(line[0], line[1], line[2]) + ") ∧ ";
+	}
+
+	result = result.slice(0, result.length - 2);
+
+	return result;
+}
+
+function createDnfForThreeVariables(truthTableAsString)
+{
+	let cnf = function (x, y, z) {
+		let result = "";
+
+		result += ((x === '0') ? "¬X" : "X") + " ∧ ";
+		result += ((y === '0') ? "¬Y" : "Y") + " ∧ ";
+		result += (z === '0') ? "¬Z" : "Z";
+
+		return result;
+	};
+
+	let result = "";
+
+	let truthTable = truthTableAsString.split('\n');
+	
+	for (let i = 0; i < truthTable.length; ++i)
+		truthTable[i] = truthTable[i].split(' ');
+
+	for (let line of truthTable) {
+		if (line[3] === '1')
+			result += "(" + cnf(line[0], line[1], line[2]) + ") ∨ ";
+	}
+
+	result = result.slice(0, result.length - 2);
+
+	return result;
+}
+
+let truthTable = createTruthTableForThreeVariables(expression);
+console.log(truthTable);
+console.log("\nСКНФ: " + createCnfForThreeVariables(truthTable));
+console.log("СДНФ: " + createDnfForThreeVariables(truthTable));
