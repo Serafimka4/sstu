4ba7e40897254ebcf0d3f0c94008fd89cf5c225e
Nefedov Danil <eanmos@ya.ru>
2020-05-05 13:11:28 +0400

    Added C# Lab №3.

---
+++ Semester №4/brovko/3/Src/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace ThirdPractice
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.printDialog1 = new System.Windows.Forms.PrintDialog();
+			this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+			this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+			this.fontDialog1 = new System.Windows.Forms.FontDialog();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.btnOpen = new System.Windows.Forms.Button();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.btnFont = new System.Windows.Forms.Button();
+			this.btnColor = new System.Windows.Forms.Button();
+			this.btnPrint = new System.Windows.Forms.Button();
+			this.lblHead = new System.Windows.Forms.Label();
+			this.lblName = new System.Windows.Forms.Label();
+			this.lblAdress = new System.Windows.Forms.Label();
+			this.lblTitle = new System.Windows.Forms.Label();
+			this.lblNumber = new System.Windows.Forms.Label();
+			this.lblPerson = new System.Windows.Forms.Label();
+			this.lblDate = new System.Windows.Forms.Label();
+			this.txtName = new System.Windows.Forms.TextBox();
+			this.txtAdress = new System.Windows.Forms.TextBox();
+			this.txtTitle = new System.Windows.Forms.TextBox();
+			this.txtNumber = new System.Windows.Forms.TextBox();
+			this.txtPerson = new System.Windows.Forms.TextBox();
+			this.txtDate = new System.Windows.Forms.TextBox();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// printDialog1
+			// 
+			this.printDialog1.UseEXDialog = true;
+			// 
+			// printPreviewDialog1
+			// 
+			this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+			this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+			this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+			this.printPreviewDialog1.Enabled = true;
+			this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+			this.printPreviewDialog1.Name = "printPreviewDialog1";
+			this.printPreviewDialog1.Visible = false;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// panel1
+			// 
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.btnOpen);
+			this.panel1.Controls.Add(this.btnExit);
+			this.panel1.Controls.Add(this.btnFont);
+			this.panel1.Controls.Add(this.btnColor);
+			this.panel1.Controls.Add(this.btnPrint);
+			this.panel1.Enabled = false;
+			this.panel1.Location = new System.Drawing.Point(52, 289);
+			this.panel1.Margin = new System.Windows.Forms.Padding(2);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(497, 69);
+			this.panel1.TabIndex = 0;
+			// 
+			// btnOpen
+			// 
+			this.btnOpen.Location = new System.Drawing.Point(43, 26);
+			this.btnOpen.Margin = new System.Windows.Forms.Padding(2);
+			this.btnOpen.Name = "btnOpen";
+			this.btnOpen.Size = new System.Drawing.Size(56, 19);
+			this.btnOpen.TabIndex = 1;
+			this.btnOpen.Text = "Open";
+			this.btnOpen.UseVisualStyleBackColor = true;
+			this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+			// 
+			// btnExit
+			// 
+			this.btnExit.Location = new System.Drawing.Point(135, 26);
+			this.btnExit.Margin = new System.Windows.Forms.Padding(2);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(56, 19);
+			this.btnExit.TabIndex = 2;
+			this.btnExit.Text = "Exit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// btnFont
+			// 
+			this.btnFont.Location = new System.Drawing.Point(226, 26);
+			this.btnFont.Margin = new System.Windows.Forms.Padding(2);
+			this.btnFont.Name = "btnFont";
+			this.btnFont.Size = new System.Drawing.Size(56, 19);
+			this.btnFont.TabIndex = 3;
+			this.btnFont.Text = "Font";
+			this.btnFont.UseVisualStyleBackColor = true;
+			this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+			// 
+			// btnColor
+			// 
+			this.btnColor.Location = new System.Drawing.Point(319, 26);
+			this.btnColor.Margin = new System.Windows.Forms.Padding(2);
+			this.btnColor.Name = "btnColor";
+			this.btnColor.Size = new System.Drawing.Size(56, 19);
+			this.btnColor.TabIndex = 4;
+			this.btnColor.Text = "Color";
+			this.btnColor.UseVisualStyleBackColor = true;
+			this.btnColor.Click += new System.EventHandler(this.btnColour_Click);
+			// 
+			// btnPrint
+			// 
+			this.btnPrint.Location = new System.Drawing.Point(407, 26);
+			this.btnPrint.Margin = new System.Windows.Forms.Padding(2);
+			this.btnPrint.Name = "btnPrint";
+			this.btnPrint.Size = new System.Drawing.Size(56, 19);
+			this.btnPrint.TabIndex = 5;
+			this.btnPrint.Text = "Print";
+			this.btnPrint.UseVisualStyleBackColor = true;
+			this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+			// 
+			// lblHead
+			// 
+			this.lblHead.AutoSize = true;
+			this.lblHead.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.lblHead.Location = new System.Drawing.Point(184, 20);
+			this.lblHead.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.lblHead.Name = "lblHead";
+			this.lblHead.Size = new System.Drawing.Size(248, 24);
+			this.lblHead.TabIndex = 6;
+			this.lblHead.Text = "Create Your Own Pamphlets";
+			// 
+			// lblName
+			// 
+			this.lblName.AutoSize = true;
+			this.lblName.Location = new System.Drawing.Point(16, 74);
+			this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.lblName.Name = "lblName";
+			this.lblName.Size = new System.Drawing.Size(125, 13);
+			this.lblName.TabIndex = 7;
+			this.lblName.Text = "Name of the organisation";
+			// 
+			// lblAdress
+			// 
+			this.lblAdress.AutoSize = true;
+			this.lblAdress.Location = new System.Drawing.Point(16, 110);
+			this.lblAdress.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.lblAdress.Name = "lblAdress";
+			this.lblAdress.Size = new System.Drawing.Size(129, 13);
+			this.lblAdress.TabIndex = 8;
+			this.lblAdress.Text = "Adress of the organisation";
+			// 
+			// lblTitle
+			// 
+			this.lblTitle.AutoSize = true;
+			this.lblTitle.Location = new System.Drawing.Point(16, 150);
+			this.lblTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.lblTitle.Name = "lblTitle";
+			this.lblTitle.Size = new System.Drawing.Size(27, 13);
+			this.lblTitle.TabIndex = 9;
+			this.lblTitle.Text = "Title";
+			// 
+			// lblNumber
+			// 
+			this.lblNumber.AutoSize = true;
+			this.lblNumber.Location = new System.Drawing.Point(310, 76);
+			this.lblNumber.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.lblNumber.Name = "lblNumber";
+			this.lblNumber.Size = new System.Drawing.Size(84, 13);
+			this.lblNumber.TabIndex = 10;
+			this.lblNumber.Text = "Contact Number";
+			// 
+			// lblPerson
+			// 
+			this.lblPerson.AutoSize = true;
+			this.lblPerson.Location = new System.Drawing.Point(310, 112);
+			this.lblPerson.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.lblPerson.Name = "lblPerson";
+			this.lblPerson.Size = new System.Drawing.Size(92, 13);
+			this.lblPerson.TabIndex = 11;
+			this.lblPerson.Text = "Person to Contact";
+			// 
+			// lblDate
+			// 
+			this.lblDate.AutoSize = true;
+			this.lblDate.Location = new System.Drawing.Point(310, 150);
+			this.lblDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.lblDate.Name = "lblDate";
+			this.lblDate.Size = new System.Drawing.Size(30, 13);
+			this.lblDate.TabIndex = 12;
+			this.lblDate.Text = "Date";
+			// 
+			// txtName
+			// 
+			this.txtName.Location = new System.Drawing.Point(166, 74);
+			this.txtName.Margin = new System.Windows.Forms.Padding(2);
+			this.txtName.Name = "txtName";
+			this.txtName.Size = new System.Drawing.Size(101, 20);
+			this.txtName.TabIndex = 13;
+			this.txtName.Leave += new System.EventHandler(this.TxtName_Leave);
+			// 
+			// txtAdress
+			// 
+			this.txtAdress.Location = new System.Drawing.Point(166, 110);
+			this.txtAdress.Margin = new System.Windows.Forms.Padding(2);
+			this.txtAdress.Name = "txtAdress";
+			this.txtAdress.Size = new System.Drawing.Size(101, 20);
+			this.txtAdress.TabIndex = 14;
+			this.txtAdress.Leave += new System.EventHandler(this.TxtAdress_Leave);
+			// 
+			// txtTitle
+			// 
+			this.txtTitle.Location = new System.Drawing.Point(166, 148);
+			this.txtTitle.Margin = new System.Windows.Forms.Padding(2);
+			this.txtTitle.Name = "txtTitle";
+			this.txtTitle.Size = new System.Drawing.Size(101, 20);
+			this.txtTitle.TabIndex = 15;
+			this.txtTitle.Leave += new System.EventHandler(this.TxtTitle_Leave);
+			// 
+			// txtNumber
+			// 
+			this.txtNumber.Location = new System.Drawing.Point(446, 71);
+			this.txtNumber.Margin = new System.Windows.Forms.Padding(2);
+			this.txtNumber.Name = "txtNumber";
+			this.txtNumber.Size = new System.Drawing.Size(103, 20);
+			this.txtNumber.TabIndex = 16;
+			this.txtNumber.Leave += new System.EventHandler(this.TxtPhone_Leave);
+			// 
+			// txtPerson
+			// 
+			this.txtPerson.Location = new System.Drawing.Point(447, 112);
+			this.txtPerson.Margin = new System.Windows.Forms.Padding(2);
+			this.txtPerson.Name = "txtPerson";
+			this.txtPerson.Size = new System.Drawing.Size(103, 20);
+			this.txtPerson.TabIndex = 17;
+			this.txtPerson.Leave += new System.EventHandler(this.TxtPerson_Leave);
+			// 
+			// txtDate
+			// 
+			this.txtDate.Location = new System.Drawing.Point(447, 148);
+			this.txtDate.Margin = new System.Windows.Forms.Padding(2);
+			this.txtDate.Name = "txtDate";
+			this.txtDate.Size = new System.Drawing.Size(103, 20);
+			this.txtDate.TabIndex = 18;
+			this.txtDate.Leave += new System.EventHandler(this.TxtDate_Leave);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(52, 193);
+			this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(498, 82);
+			this.richTextBox1.TabIndex = 19;
+			this.richTextBox1.Text = "";
+			// 
+			// errorProvider1
+			// 
+			this.errorProvider1.ContainerControl = this;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(586, 369);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.txtDate);
+			this.Controls.Add(this.txtPerson);
+			this.Controls.Add(this.txtNumber);
+			this.Controls.Add(this.txtTitle);
+			this.Controls.Add(this.txtAdress);
+			this.Controls.Add(this.txtName);
+			this.Controls.Add(this.lblDate);
+			this.Controls.Add(this.lblPerson);
+			this.Controls.Add(this.lblNumber);
+			this.Controls.Add(this.lblTitle);
+			this.Controls.Add(this.lblAdress);
+			this.Controls.Add(this.lblName);
+			this.Controls.Add(this.lblHead);
+			this.Controls.Add(this.panel1);
+			this.Margin = new System.Windows.Forms.Padding(2);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.panel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+#endregion
+
+		private System.Windows.Forms.PrintDialog printDialog1;
+		private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        	private System.Drawing.Printing.PrintDocument printDocument1;
+		private System.Windows.Forms.FontDialog fontDialog1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.ColorDialog colorDialog1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button btnOpen;
+		private System.Windows.Forms.Button btnFont;
+		private System.Windows.Forms.Button btnColor;
+		private System.Windows.Forms.Button btnPrint;
+		private System.Windows.Forms.Label lblName;
+		private System.Windows.Forms.Label lblAdress;
+		private System.Windows.Forms.Label lblTitle;
+		private System.Windows.Forms.Label lblNumber;
+		private System.Windows.Forms.Label lblPerson;
+		private System.Windows.Forms.Label lblDate;
+		private System.Windows.Forms.TextBox txtAdress;
+		private System.Windows.Forms.TextBox txtTitle;
+		private System.Windows.Forms.TextBox txtNumber;
+		private System.Windows.Forms.TextBox txtPerson;
+		private System.Windows.Forms.TextBox txtDate;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.ErrorProvider errorProvider1;
+		public System.Windows.Forms.TextBox txtName;
+		public System.Windows.Forms.Label lblHead;
+		protected System.Windows.Forms.Button btnExit;
+	}
+}
+
