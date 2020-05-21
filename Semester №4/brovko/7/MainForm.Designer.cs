9af697d2f2c324e89aa7b0b4191f63addaf9b04c
Nefedov Danil <eanmos@ya.ru>
2020-05-21 14:44:21 +0400

    Added Brovko's Lab №7.

---
+++ Semester №4/brovko/7/MainForm.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Lab_7
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripSeparator1,
+            this.toolStripButton2,
+            this.toolStripSeparator2,
+            this.toolStripButton3,
+            this.toolStripSeparator3,
+            this.toolStripButton4,
+            this.toolStripSeparator4,
+            this.toolStripButton5});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(648, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(52, 22);
+            this.toolStripButton1.Text = "&TextBox";
+            this.toolStripButton1.Click += new System.EventHandler(this.ToolStripButton1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(39, 22);
+            this.toolStripButton2.Text = "&Label";
+            this.toolStripButton2.Click += new System.EventHandler(this.ToolStripButton2_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(64, 22);
+            this.toolStripButton3.Text = "&CheckBox";
+            this.toolStripButton3.Click += new System.EventHandler(this.ToolStripButton3_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(71, 22);
+            this.toolStripButton4.Text = "&ComboBox";
+            this.toolStripButton4.Click += new System.EventHandler(this.ToolStripButton4_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(47, 22);
+            this.toolStripButton5.Text = "&Button";
+            this.toolStripButton5.Click += new System.EventHandler(this.ToolStripButton5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.Location = new System.Drawing.Point(9, 24);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel1.Size = new System.Drawing.Size(452, 382);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.textBox5);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(477, 25);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(157, 381);
+            this.panel2.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 272);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Text";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(64, 269);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(76, 20);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.TextChanged += new System.EventHandler(this.TextBox5_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(64, 222);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(76, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextChanged += new System.EventHandler(this.TextBox4_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(64, 177);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(64, 131);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 85);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 180);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 225);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Height";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 134);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 88);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "X";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Properties";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(47, 341);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 412);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Semester №4/brovko/7/MainForm.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab_7
+{
+    public partial class MainForm : Form
+    {
+        private Control selectedElement;
+        private readonly int defaultXCoord = 20;
+        private readonly int defaultYCoord = 20;
+        private readonly ElementFactory elementFactory;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            elementFactory = new ElementFactory(defaultXCoord, defaultYCoord);
+        }
+
+        private void ToolStripButton1_Click(object sender, EventArgs e)
+        {
+            AddElementToPanelAndAddHadler(elementFactory.CreateTextBox());
+        }
+
+        private void ToolStripButton2_Click(object sender, EventArgs e)
+        {
+            AddElementToPanelAndAddHadler(elementFactory.CreateLabel());
+        }
+
+        private void ToolStripButton3_Click(object sender, EventArgs e)
+        {
+            AddElementToPanelAndAddHadler(elementFactory.CreateCheckBox());
+        }
+
+        private void ToolStripButton4_Click(object sender, EventArgs e)
+        {
+            AddElementToPanelAndAddHadler(elementFactory.CreateComboBox());
+        }
+
+        private void ToolStripButton5_Click(object sender, EventArgs e)
+        {
+            AddElementToPanelAndAddHadler(elementFactory.CreateButton());
+
+        }
+
+        private void AddElementToPanelAndAddHadler(Control component)
+        {
+            component.MouseClick += new MouseEventHandler(SelectOnClick);
+            panel1.Controls.Add(component);
+        }
+
+        private void SelectOnClick(object sender, MouseEventArgs e)
+        {
+            selectedElement = sender as Control;
+
+            textBox1.Text = selectedElement.Location.X.ToString();
+            textBox2.Text = selectedElement.Location.Y.ToString();
+            textBox3.Text = selectedElement.Width.ToString();
+            textBox4.Text = selectedElement.Height.ToString();
+            textBox5.Text = selectedElement.Text;
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (ExistSelected())
+                selectedElement.Location = new Point(ChangedValue(textBox1), selectedElement.Location.Y);
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (ExistSelected())
+                selectedElement.Location = new Point(selectedElement.Location.X, ChangedValue(textBox2));
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (ExistSelected())
+                selectedElement.Width = ChangedValue(textBox3);
+        }
+
+        private void TextBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (ExistSelected())
+                selectedElement.Height = ChangedValue(textBox4);
+        }
+
+        private int ChangedValue(Control textField) 
+        {
+            if (!int.TryParse(textField.Text, out int newValue) && textField.Text != "" && textField.Text != "-")
+            {
+                textField.Text = 0.ToString();
+                UncorrectMessageShow();
+            }
+
+            return newValue;
+        }
+
+        private void TextBox5_TextChanged(object sender, EventArgs e)
+        {
+            if (ExistSelected())
+                selectedElement.Text = textBox5.Text;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (ExistSelected())
+            {
+                panel1.Controls.Remove(selectedElement);
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+            }
+        }
+
+        private bool ExistSelected()
+        {
+            return selectedElement != null;
+        }
+
+        private void UncorrectMessageShow()
+        {
+            MessageBox.Show("Некоректное значение");
+        }
+    }
+
+    class ElementFactory
+    {
+        private readonly int defaultXCoord;
+        private readonly int defaultYCoord;
+        private readonly ElementChangeMethods methods = new ElementChangeMethods();
+
+        public ElementFactory(int defaultXCoord, int defaultYCoord)
+        {
+            this.defaultXCoord = defaultXCoord;
+            this.defaultYCoord = defaultYCoord;
+        }
+
+        public TextBox CreateTextBox()
+        {
+            TextBox newTextBox = new TextBox
+            {
+                Location = new Point(defaultXCoord, defaultYCoord)
+            };
+
+            AddEventHandlers(newTextBox);
+            return newTextBox;
+        }
+
+        public Label CreateLabel()
+        {
+            Label newLabel = new Label
+            {
+                BorderStyle = BorderStyle.FixedSingle,
+                Location = new Point(defaultXCoord + 30, defaultYCoord + 30)
+            };
+
+            AddEventHandlers(newLabel);
+            return newLabel;
+        }
+
+        public CheckBox CreateCheckBox()
+        {
+            CheckBox newCheckBox = new CheckBox
+            {
+                Location = new Point(defaultXCoord + 60, defaultYCoord + 60)
+            };
+
+            AddEventHandlers(newCheckBox);
+            return newCheckBox;
+        }
+
+        public ComboBox CreateComboBox()
+        {
+            ComboBox newComboBox = new ComboBox
+            {
+                Location = new Point(defaultXCoord + 80, defaultYCoord + 80)
+            };
+
+            AddEventHandlers(newComboBox);
+            return newComboBox;
+        }
+
+        public Button CreateButton()
+        {
+            Button newButton = new Button
+            {
+                BackColor = Color.White,
+                ForeColor = Color.Black,
+                Location = new Point(defaultXCoord + 100, defaultYCoord + 100)
+            };
+
+            AddEventHandlers(newButton);
+            return newButton;
+        }
+
+        private void AddEventHandlers(Control element)
+        {
+            element.MouseDown += new MouseEventHandler(methods.MouseDown);
+            element.MouseUp += new MouseEventHandler(methods.MouseUp);
+            element.MouseMove += new MouseEventHandler(methods.MouseMove);
+        }
+    }
+
+    class ElementChangeMethods
+    {
+        private Control selectedElem;
+        private int deltaX;
+        private int deltaY;
+
+        public void MouseDown(object component, MouseEventArgs e)
+        {
+            Control element = component as Control;
+
+            selectedElem = element;
+
+            deltaX = Cursor.Position.X - element.Location.X;
+            deltaY = Cursor.Position.Y - element.Location.Y;
+        }
+
+        public void MouseUp(object component, MouseEventArgs e)
+        {
+            selectedElem = null;
+        }
+
+        public void MouseMove(object component, MouseEventArgs e)
+        {
+            Control element = component as Control;
+
+            if (selectedElem == element)
+            {
+                element.Location = new Point(
+                   Cursor.Position.X - deltaX,
+                   Cursor.Position.Y - deltaY
+                );
+            }
+        }
+    }
+}
